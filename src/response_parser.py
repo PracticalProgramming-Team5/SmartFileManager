af8b4ab4f8f26795b0777fab351166f2c1daafc8
Juhyunsoo <amature0000@naver.com>
2025-04-27 17:14:13 +0900

    add _check_command

--- src/response_parser.py
+++ src/response_parser.py
@@ -1,13 +1,24 @@
 import re
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Sequence
 from pydantic import BaseModel, ValidationError, TypeAdapter
 import json
 import logging
+from settings_manager import SettingsManager
+from pathlib import Path
+
 """
 JHS
 """
+
 # TODO: 나중에 context_builder.py로 이동
 # json 파싱 형식 정의
+class ActionMove(BaseModel):
+    source: str
+    destination: Tuple[str]
+    explanation: Tuple[str]
+    def __repr__(self):
+        pass # TODO: 프롬프트 입력에 사용될 repr
+
 class ActionCommand(BaseModel):
     action: str
     source: str
@@ -43,8 +54,36 @@ def _extract_json(text: str) -> str:
     if match: return match.group(1)
     raise ValueError("no json block in response str.")
 
-def _check_commands(restrict : Tuple, *args):
-    pass # TODO: restrict 안에 있는 action만 호출하는지 검사
+def _check_command(restrict : Tuple, cmd : Dict) -> bool:
+    """
+    해석된 명령이 타당한지 검사
+    1. action이 허용된 api로만 구성되는가?
+    2. source / destination이 감시 중인 디렉토리의 하위 폴더인가?
+    """
+    action = cmd.get("action")
+    if action and action not in restrict:
+        _logger.error(f"wrong action called: {action}")
+        return False
+    
+    observing_dirs = SettingsManager.get("available_dirs")
+    base_paths = [Path(d).resolve() for d in observing_dirs]
+
+    for key in ("source", "destination"):
+        k = cmd.get(key)
+        if not k: continue
+
+        paths = None
+        if isinstance(k, str):
+            paths = [k]
+        elif isinstance(k, Sequence):
+            paths = k
+        else: return False
+
+        for p in paths:
+            p=Path(p).resolve()
+            if not any(p.is_relative_to(base) for base in base_paths):
+                return False
+    return True
 
 class ResponseParser:
     """
@@ -53,7 +92,7 @@ class ResponseParser:
     Note: 응답은 \`\`\` json ... ``` 구조를 가져야 합니다.
     """
     actionlist_adapter = TypeAdapter(ActionCommandList)
-    action_adapter = TypeAdapter(ActionCommand)
+    actionmove_adapter = TypeAdapter(ActionMove)
 
     @staticmethod
     def parse_action_move(llm_response: str) -> Dict[str,str] | None:
@@ -68,14 +107,15 @@ class ResponseParser:
         """
         try:
             json_block = _extract_json(llm_response)
-            val_data = ResponseParser.action_adapter.validate_json(json_block)
+            val_data = ResponseParser.actionmove_adapter.validate_json(json_block)
             command = val_data.model_dump()
-            print(command)
+            # print(command)
+            # print(type(command))
+            if _check_command({'move', 'rename', 'copy'}, command): 
+                return command
         except (ValueError, ValidationError, json.JSONDecodeError) as e:
             _logger.error(f"{type(e).__name__}: {e}")
-            return None
-        # TODO: 실행 타당성 검사
-        # if _check_commands({'move', 'rename', 'copy'}, command): ...
+        return None
 
     @staticmethod
     def parse_action_command(llm_response: str) -> List[Dict[str,str]] | None:
@@ -92,13 +132,16 @@ class ResponseParser:
             json_block = _extract_json(llm_response)
             val_data = ResponseParser.actionlist_adapter.validate_json(json_block)
             commands = val_data.model_dump()
-            print(commands)
+            # print(commands)
+            # print(type(commands))
+            restrict = SettingsManager.get("available_apis")
+            if all(_check_command(restrict, command) for command in commands):
+                return commands
         except (ValueError, ValidationError, json.JSONDecodeError) as e:
             _logger.error(f"{type(e).__name__}: {e}")
-            return None
-        # TODO: 실행 타당성 검사
+        return None
 
-# # 예제 코드
+# # 예제 코드. SettingsManager 미구현 시 실행 과정에서 오류가 발생함.
 # action_str = """```json{
 #   "plan": [
 #     {
@@ -144,8 +187,8 @@ class ResponseParser:
 #     "destination": "대상 경로"
 # }"""
 # if __name__ == "__main__":
-    # ResponseParser.parse_action_command(action_str)
-    # ResponseParser.parse_action_move(move_str)
-    # print("다른 구조의 응답 파싱")
-    # ResponseParser.parse_action_command(action_str2)
-    # ResponseParser.parse_action_move(move_str2)
+#     ResponseParser.parse_action_command(action_str)
+#     ResponseParser.parse_action_move(move_str)
+#     print("다른 구조의 응답 파싱")
+#     ResponseParser.parse_action_command(action_str2)
+#     ResponseParser.parse_action_move(move_str2)
