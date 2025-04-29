81c878401d5b38f3d2670e09b0bd69b39dc254bb
spicykong <ssw30951@gmail.com>
2025-04-29 14:35:19 +0900

    콜백함수 등록 인터페이스 추가
    필수 구현에 필요한 이벤트들을 처리하는 콜백함수 등록 인터페이스 추가

--- src/ui_manager.py
+++ src/ui_manager.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 class UIManager:
     """
     그래픽 사용자 인터페이스(GUI) 요소들을 관리하고 사용자와의 상호작용을 처리합니다.
@@ -31,16 +33,59 @@ class UIManager:
 
     def display_window_command(self) -> None:
         """
-        명령 입력 윈도우를 띄움
+        명령 입력 윈도우를 띄움.
         """
         pass
 
     def display_window_notificaiton(self, message: str, message_type: int) -> None:
         """
-        알림 창을 띄움
+        알림 창을 띄움.
 
         Args:
             message: 표시할 메시지
             message_type: 메시지 유형 (성공, 알림, 경고, 실패 등.. 상의 필요)
         """
         pass
+
+    def register_on_command_submit(self, callback: Callable[[str], None]):
+        """
+        명령어 입력 이벤트를 처리하는 콜백 함수 등록.
+        
+        Args:
+            callback(command: str) -> None: command 문자열을 받아 처리하는 콜백 함수
+                - command: 사용자가 입력한 명령어
+                
+        """
+        pass
+
+    def register_on_suggestion_accepted(self, callback: Callable[[str, str], None]):
+        """
+        파일 이동 요청 이벤트를 처리하는 콜백 함수 등록
+        
+        Args:
+            callback(src_pth: str, dst_pth: str) -> None: 파일 이동을 처리하는 콜백 함수
+                - src_pth: 원래 파일 경로
+                - dst_pth: 옮길 파일 경로
+
+        """
+        pass
+
+    def register_on_undo_clicked(self, callback: Callable[[], None]):
+        """
+        명령 취소 이벤트를 처리하는 콜백 함수 등록
+        
+        Args:
+            callback() -> None: 최근 명령을 취소하는 콜백 함수
+        """
+        pass
+
+    def register_on_settings_applied(self, callback: Callable[[str], None]):
+        """
+        설정 적용 이벤트를 처리하는 콜백 함수 등록
+        
+        Args:
+            callback(settings_json: str) -> None: settings 변경 사항을 적용하는 콜백 함수
+                setting_json: 설정 파일 내용
+        """
+        pass
+    
