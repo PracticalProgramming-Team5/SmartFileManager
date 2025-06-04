1b60df2e2305b4b764b699c48f8d9eac1521bdd7
spicykong <ssw30951@gmail.com>
2025-06-05 01:53:15 +0900

    Add status widget
    - status창 추가
    - 스크롤바 qss 추가
    - 라이프사이클 정의

--- src/ui_quick_window.py
+++ src/ui_quick_window.py
@@ -1,14 +1,22 @@
-from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QLineEdit, QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QScrollArea
 from PyQt5.QtCore import Qt, pyqtSignal, QThread, QTimer, QFile, QTextStream
 from PyQt5.QtGui import QCursor
 import sys
 from pynput import keyboard
 import platform
+from enum import Enum
+
+class LifeCycle(Enum):
+    TYPING = 0 # 사용자로부터 입력을 받는 상태
+    SUBMITTED = 1 # 사용자 입력을 LLM에게 제출한 상태
+    RESPONDED = 2 # LLM으로 부터 응답이 온 상태
+    OPERATING = 3 # 사용자가 동작을 수락하고 동작중인 상태
 
 COMBO = {keyboard.Key.shift, keyboard.Key.space}
 COMBO2 = {keyboard.Key.cmd, keyboard.Key.shift}
 
 PATH_STYLE_SHEET = "quick_style.qss"
+
 
 class GlobalHotKeyThread(QThread):
     hotkey_pressed = pyqtSignal()
@@ -43,18 +51,10 @@ class GlobalHotKeyThread(QThread):
 class QuickInput(QLineEdit):
     def __init__(self):
         super().__init__()
+        self.setObjectName("QuickInput")
         self.setFixedHeight(50)
         self.setFixedWidth(600)
         self.setPlaceholderText("Type something...")
-        self.setObjectName("QuickInput")
-        # self.setStyleSheet("""
-        #         outline: none;
-        #         background-color: #f0f0f0;
-        #         border: 1px solid #ccc;
-        #         border-radius: 10px;
-        #         color: #000000;
-        #         font-size: 24px;
-        # """)
     
     def keyPressEvent(self, event):
         if self.text() and event.key() == Qt.Key_Escape:
@@ -62,15 +62,69 @@ class QuickInput(QLineEdit):
         else:
             super().keyPressEvent(event)
 
+class FocusableBtn(QPushButton):
+    def __init__(self, text):
+        super().__init__()
+        self.setObjectName("FocusableBtn")
+        self.setFocusPolicy(Qt.StrongFocus)
+        self.setText(text)
+
+class ScrollableLabel(QLabel):
+    def __init__(self):
+        super().__init__()
+        self.setObjectName("ScrollableLabel")
+        self.setWordWrap(True)
+        # self.setAttribute(Qt.WA_StyledBackground, True)
+        self.scroll = QScrollArea()
+        self.scroll.setWidget(self)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.scroll.setMinimumHeight(5)
+        self.scroll.setMaximumHeight(80)
+        self.scroll.setFocusPolicy(Qt.NoFocus)
+    
+    def get_scroll(self):
+        return self.scroll
+
 class LLMStatusWidget(QWidget):
     def __init__(self):
         super().__init__()
-    
+        self.setObjectName("LLMStatusWidget")
+
+        self.setAttribute(Qt.WA_TranslucentBackground, False)
+        self.setAttribute(Qt.WA_StyledBackground, True)
+
+        layout = QVBoxLayout(self)
+
+        self.message = ScrollableLabel()
+        self.feature = ScrollableLabel()
+
+        form_btn = QWidget()
+
+        layout_btn = QHBoxLayout(form_btn)
+
+        self.btn_cancle = FocusableBtn("Cancle")
+        self.btn_ok = FocusableBtn("Ok")
+
+        self.btn_cancle.setEnabled(False)
+        self.btn_ok.setEnabled(False)
+
+        layout_btn.addWidget(self.btn_cancle, stretch=1)
+        layout_btn.addWidget(self.btn_ok, stretch=2)
+        
+        self.btn_ok.clicked.connect(self.hide)
+
+        layout.addWidget(QLabel("LLM Response"))
+        layout.addWidget(self.message.get_scroll())
+        layout.addWidget(QLabel("Features"))
+        layout.addWidget(self.feature.get_scroll()) 
+        layout.addWidget(form_btn)
+
     
 class InstantWindow(QWidget):
     def __init__(self):
         super().__init__()
-        
+        self.state = LifeCycle.TYPING
         self.isWinsOs = (platform.system() == "Windows")
         self.setWindowFlags(Qt.Tool | Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
@@ -82,11 +136,12 @@ class InstantWindow(QWidget):
         QApplication.instance().aboutToQuit.connect(self.listener_thread.quit)
 
 
-        layout = QVBoxLayout()
+        layout = QVBoxLayout(self)
         self.input = QuickInput()
+        self.output = QWidget()
         # self.input.closed.connect()
         layout.addWidget(self.input)
-        self.setLayout(layout)
+        layout.addWidget(LLMStatusWidget())
         
         # self.resize(400, 500) 
         
@@ -94,6 +149,10 @@ class InstantWindow(QWidget):
         # 포커스 변경 감지
         QApplication.instance().focusChanged.connect(self.__on_focus_change)
     
+    def __submit(self):
+        print(self.input.text()) 
+        # 라이프 사이클 관리 추가 해야 함.
+
     def display_window(self):
 
         if self.isVisible():
@@ -113,7 +172,7 @@ class InstantWindow(QWidget):
         if event.key() == Qt.Key_Escape:
             self.hide()
         elif event.key() == Qt.Key_Return:
-            print(self.input.text())
+            self.__submit()
         else:
             super().keyPressEvent(event)  
 
