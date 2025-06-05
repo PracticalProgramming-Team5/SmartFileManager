73ca4eeafbaad13710a53955983633db409d22f8
Juhyunsoo <amature0000@naver.com>
2025-06-05 20:15:08 +0900

    simple change

--- src/history_manager.py
+++ src/history_manager.py
@@ -1,19 +1,21 @@
 class HistoryManager:
     def __init__(self, max_history: int = 20):
-        self.history = []
-        self.max_history = max_history
+        self._history = []
+        self.__max_history = max_history
 
-    def log_action(self, action_details: dict):
-        if len(self.history) >= self.max_history:
-            self.history.pop(0)  # 가장 오래된 항목 제거
-        self.history.append(action_details)
+    def log(self, action_details: dict):
+        if len(self._history) >= self.__max_history:
+            self._history.pop(0)  # 가장 오래된 항목 제거
+        self._history.append(action_details)
 
-    def get_last_action(self) -> dict | None:
-        if self.history:
-            return self.history[-1]
+    def peek(self) -> dict | None:
+        if self._history:
+            return self._history[-1]
         return None
 
-    def pop_last_action(self) -> dict | None:
-        if self.history:
-            return self.history.pop()
-        return None
+    def delete(self, index) -> None:
+        if index >= 0 and self._history and len(self._history) > index:
+            del self._history[index]
+
+    def get(self) -> list:
+        return self._history
